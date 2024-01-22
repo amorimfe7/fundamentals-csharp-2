1ad13b07812d817ce29e4a3e632f3013ea52f92b
amorimfe7 <amorimfe7@gmail.com>
2024-01-22 19:33:02 -0300

    Comparando Strings

---
+++ Strings/ComparaString/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ComparaString{
+    class Program{
+        static void Main(string[] args){
+            //ComparteTo retorna 0 e 1
+            var text1 = "Testando a Comparação";
+            var text2 = "Testando 2"; 
+            // Console.WriteLine(text1.CompareTo("testando"));
+
+            //Contains | Contém > retorna True or False
+            Console.WriteLine(text1.Contains("teste"));
+            Console.WriteLine(text1.Contains("testando", StringComparison.OrdinalIgnoreCase)); //ignora case sensitive
+        }
+    }
+}
+

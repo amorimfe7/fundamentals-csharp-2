e1f134ca5e586c0c25901dbd4b3beed38317a11f
amorimfe7 <amorimfe7@gmail.com>
2024-01-22 18:08:04 -0300

    Guids

---
+++ Guids/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MyApp{
+    class Program{
+        static void Main(string[] args){
+            // var id = Guid.NewGuid(); //gerar um novo GUID
+
+            var id = new Guid("82ced55f-f896-41bb-82ef-9bd6a065d94c"); //assimilando um GUID
+            var id2 = Guid.NewGuid(); //gerar um novo GUID
+
+            //comparando grids
+            if (id == id2){
+                Console.WriteLine("Os Grids são iguais");
+            } else {
+                Console.WriteLine(id.ToString().Substring(0,8));
+                Console.WriteLine(id2.ToString().Substring(0,8));
+                Console.WriteLine("Os Grids não são iguais");
+            }
+        }
+    }
+}

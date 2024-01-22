9c363753ebfaa5780f160731d2488522ce53984f
amorimfe7 <amorimfe7@gmail.com>
2024-01-22 19:55:06 -0300

    Equals

---
+++ Strings/Equals/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Equals{
+    class Program{
+        static void Main(string[] args){
+            //Equals | Exatamente igual
+            var text = "Testando comparação com Equals";
+            Console.WriteLine(text.Equals("Testando comparação com equals"));
+            Console.WriteLine(text.Equals("Testando comparação com Equals"));
+        }
+    }
+}

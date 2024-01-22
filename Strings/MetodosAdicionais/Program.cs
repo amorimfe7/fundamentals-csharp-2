9170c9ecd7695aed3f213f93adeeaa43c2608b51
amorimfe7 <amorimfe7@gmail.com>
2024-01-22 20:34:00 -0300

    Metodos Adicionais

---
+++ Strings/MetodosAdicionais/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MetodosAdicionais{
+    class Program{
+        static void Main(string[] args){
+
+            var text = "Este é um texto criado para teste";
+
+            Console.WriteLine(text.ToLower());
+            Console.WriteLine(text.ToUpper());
+            // Console.WriteLine(text.Insert(7, "INSERIDO"));
+            Console.WriteLine(text.Remove(2, 2)); //começar no indice 2 e remover os próximos 2 caracteres
+            Console.WriteLine(text.Length);
+        }
+    }
+}

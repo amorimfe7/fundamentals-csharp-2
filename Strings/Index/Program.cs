3ffd17866084f32c37348444c928e476e49d501b
amorimfe7 <amorimfe7@gmail.com>
2024-01-22 20:01:40 -0300

    IndexOf

---
+++ Strings/Index/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Index{
+    class Program{
+        static void Main(string[] args){
+            //Index/Índice da lista
+            var text = "Testando o Index";
+            var position = (text.IndexOf("o")); //IndexOf sempre vai pedir um obj do mesmo tipo
+            var lastPosition = (text.LastIndexOf("x"));
+
+            Console.WriteLine($"O índice desejado está na posição {position}");
+            Console.WriteLine($"O último índice utilizando o filtro desejado está na posição {lastPosition}");
+        }
+    }
+}

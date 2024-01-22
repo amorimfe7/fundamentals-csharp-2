c8e903e301046fee0a4d2a59ba72970f9e9db3af
amorimfe7 <amorimfe7@gmail.com>
2024-01-22 18:39:12 -0300

    Strings

---
+++ Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Strings{
+    class Program{
+        static void Main(string[] args){
+            var price = 10.2;
+            //Concatenação
+                var text0 = "O preço do produto é: " + price;
+                Console.WriteLine(text0);
+
+            //String.Format();
+                var promocao = true;
+                var text1 = string.Format("O preço do produto é: {0}, a promoção é {1}", price, promocao);
+                Console.WriteLine(text1);
+
+            // $"" | $@ significa que tudo é uma string
+            var text2 = $@"O preço do produto é {price}
+            apenas na promoção";
+            Console.WriteLine(text2);
+        }
+    }
+}

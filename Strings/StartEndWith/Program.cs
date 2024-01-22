8ee3c403957ed61502eae497a63fb18d7ca9ebab
amorimfe7 <amorimfe7@gmail.com>
2024-01-22 19:45:58 -0300

    StartsEnds With

---
+++ Strings/StartEndWith/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace StartEndWith
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            //StartsWith aplica-se IgnoreCase
+            var text = "Testando comparação e with";
+            Console.WriteLine(text.StartsWith("Testando"));
+            Console.WriteLine(text.StartsWith("testando"));
+
+            //EndsWith
+            Console.WriteLine(text.EndsWith("with"));
+            Console.WriteLine(text.EndsWith("fim"));
+        }
+    }
+}

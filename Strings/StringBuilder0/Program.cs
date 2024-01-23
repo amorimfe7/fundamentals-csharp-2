4ab33225192830c70b962a1d3e0de34473638397
amorimfe7 <amorimfe7@gmail.com>
2024-01-23 11:55:16 -0300

    StringBuilder

---
+++ Strings/StringBuilder0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilder0{
+    class Program{
+        static void Main(string[] args){
+            //quando possuimos textos mt grandes, uma das opções é utilizar o @, pois, vai ler a string completa
+            //como se fosse apenas 1 line >> poupa memória
+
+            //se precisar criar as linhas de forma dinâmica, o ideal é que utilize o StringBuilder
+
+            var text = "Este é um texto teste";
+            text += " AQUI";
+
+            var text2 = new StringBuilder(); //construção de texto
+            text2.ToString(); //dentro de um outro tipo de objeto, mt provável q tenhamos q converter .ToString()
+                                //aqui não foi necessário, pois, o WriteLine já realiza a conversão
+
+            text2.Append("Testando ");
+            text2.Append("a ");
+            text2.Append("frase ");
+            text2.Append("teste");
+            Console.WriteLine(text2);
+
+        }
+    }
+}
